6335e19fb8b2083069acf988fbe2215502e2d05c
dachen123 <2391840838@qq.com>
2016-08-14 17:09:57 +0800

    viapay test

---
+++ payment/viapay/notify.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * [WeEngine System] Copyright (c) 2014 WE7.CC
+ * WeEngine is NOT a free software, it under the license terms, visited http://www.we7.cc/ for more details.
+ */
+define('IN_MOBILE', true);
+require '../../framework/bootstrap.inc.php';
+require '../../app/common/bootstrap.app.inc.php';
+load()->app('common');
+load()->app('template');
+
+
+message("notify");
+?>
+
+
---
+++ payment/viapay/return.php
@@ -0,0 +1,10 @@
+<?php
+define('IN_MOBILE', true);
+require '../../framework/bootstrap.inc.php';
+require '../../app/common/bootstrap.app.inc.php';
+load()->app('common');
+load()->app('template');
+
+
+message("return");
+?>
